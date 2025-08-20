1c6d662b71ca2885d4a71e68063d7234aea37f2e
Alexander Lomakin <aglomakin@gmail.com>
2025-08-20 14:46:46 +0300

    added focusing mode

--- src/ImageViz.cpp
+++ src/ImageViz.cpp
@@ -1,3 +1,4 @@
+
 #include <atomic>
 #include <chrono>
 #include <cmath>
@@ -6,16 +7,19 @@
 #include <cstdio>
 #include <cstdlib>
 #include <deque>
+#include <fmt/color.h>
 #include <fmt/format.h>
+#include <future>
 #include <memory>
 #include <mutex>
-#include <opencv2/core/matx.hpp>
+#include <opencv2/core.hpp>
 #include <opencv2/core/types.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <optional>
 #include <rclcpp/callback_group.hpp>
+#include <rclcpp/client.hpp>
 #include <rclcpp/executors.hpp>
 #include <rclcpp/executors/multi_threaded_executor.hpp>
 #include <rclcpp/logging.hpp>
@@ -25,6 +29,8 @@
 #include <rclcpp/subscription.hpp>
 #include <rclcpp/subscription_options.hpp>
 #include <rclcpp/utilities.hpp>
+#include <rosbag2_interfaces/srv/pause.hpp>
+#include <rosbag2_interfaces/srv/resume.hpp>
 #include <sensor_msgs/msg/compressed_image.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/imu.hpp>
@@ -32,6 +38,8 @@
 #include <shared_mutex>
 #include <string_view>
 #include <thread>
+
+using namespace std::chrono_literals;
 
 class FPSMeter {
 public:
@@ -77,9 +85,12 @@ class ImageViz : public rclcpp::Node {
 public:
   ImageViz() : rclcpp::Node{"cpp_img_viz"} {
 
-    img_sub_ = create_subscription<sensor_msgs::msg::CompressedImage>(
-        "/camera/image_raw/compressed", 10,
-        [this](sensor_msgs::msg::CompressedImage::UniquePtr msg) {
+    img_size_.width = 640;
+    img_size_.height = 400;
+
+    img_sub_ = create_subscription<sensor_msgs::msg::Image>(
+        "/camera/image_raw", 10,
+        [this](sensor_msgs::msg::Image::UniquePtr msg) {
           img_fps_.tick();
 
           std::lock_guard lock{img_protector_};
@@ -104,10 +115,20 @@ public:
           gps_fps_.tick();
         });
 
+    resume_ = create_client<rosbag2_interfaces::srv::Resume>(
+        "/rosbag2_recorder/resume");
+    pause_ = create_client<rosbag2_interfaces::srv::Pause>(
+        "/rosbag2_recorder/pause");
+
     wt_ = std::make_unique<std::thread>([this]() {
       cv::namedWindow("debug", cv::WINDOW_FULLSCREEN);
       cv::setWindowProperty("debug", cv::WND_PROP_FULLSCREEN,
                             cv::WINDOW_FULLSCREEN);
+
+      auto record_start_time{std::chrono::system_clock::now()};
+      auto last_blink_time{std::chrono::system_clock::now()};
+      bool blink_on{false};
+
       while (not stop_requested_) {
 
         bool queue_empty{true};
@@ -121,7 +142,7 @@ public:
           continue;
         }
 
-        sensor_msgs::msg::CompressedImage::UniquePtr msg;
+        sensor_msgs::msg::Image::UniquePtr msg;
         {
           std::lock_guard lock{img_protector_};
           msg = std::move(img_queue_.front());
@@ -129,15 +150,89 @@ public:
         }
 
         cv::Mat_<cv::Vec3b> img =
-            cv::imdecode({msg->data.data(), static_cast<int>(msg->data.size())},
-                         cv::IMREAD_UNCHANGED);
+            cv::Mat_<uint8_t>{msg->data, true}.reshape(3, msg->height);
 
-        cv::resize(img, img, {640, 400});
+        if (focusing_mode_) {
+          img = img(
+              cv::Rect((img.cols >> 1) - 100, (img.rows >> 1) - 100, 200, 200));
 
-        draw(img);
+          cv::Mat_<uint8_t> img8b;
+          cv::cvtColor(img, img8b, cv::COLOR_BGR2GRAY);
+
+          cv::Mat_<float> laplacian_img;
+          cv::Laplacian(img8b, laplacian_img, CV_32FC1, 5);
+          const auto sharpness_metric{cv::mean(cv::abs(laplacian_img))};
+
+          draw_text(img, fmt::format("{:.1f}", sharpness_metric(0)), {10, 25},
+                    {0.0, 0.0, 255.0});
+        } else {
+          cv::resize(img, img, img_size_, 0.0, 0.0, cv::INTER_NEAREST);
+          cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
+          draw(img);
+
+          if (record_started_) {
+            const auto current_time{std::chrono::system_clock::now()};
+
+            if ((current_time - last_blink_time) > 100ms) {
+              last_blink_time = current_time;
+              blink_on = not blink_on;
+
+              if (blink_on) {
+                cv::circle(img, {50, 350}, 20, {0.0, 0.0, 255.0}, cv::FILLED,
+                           cv::LINE_AA);
+              }
+            }
+
+            const auto record_time{
+                std::chrono::duration_cast<std::chrono::seconds>(
+                    current_time - record_start_time)
+                    .count()};
+
+            draw_text(img, fmt::format("{} s", record_time), {80, 355},
+                      {0.0, 255.0, 0.0});
+          }
+        }
 
         cv::imshow("debug", img);
-        cv::waitKey(1);
+
+        const auto key_pressed{cv::waitKey(1)};
+
+        if (key_pressed == ' ' and not focusing_mode_) {
+
+          if (not record_started_) {
+
+            auto res{resume_->async_send_request(
+                std::make_shared<rosbag2_interfaces::srv::Resume::Request>())};
+
+            if (res.wait_for(5s) == std::future_status::ready) {
+              RCLCPP_INFO(get_logger(), "%s",
+                          fmt::format(fmt::fg(fmt::color::green_yellow),
+                                      "Recording started..")
+                              .c_str());
+
+              record_started_ = true;
+              record_start_time = std::chrono::system_clock::now();
+            }
+
+          } else {
+
+            auto res{pause_->async_send_request(
+                std::make_shared<rosbag2_interfaces::srv::Pause::Request>())};
+
+            if (res.wait_for(5s) == std::future_status::ready) {
+              RCLCPP_INFO(get_logger(), "%s",
+                          fmt::format(fmt::fg(fmt::color::green_yellow),
+                                      "Recording stopped..")
+                              .c_str());
+
+              record_started_ = false;
+            }
+          }
+        }
+
+        if (key_pressed == 'f' and not record_started_) {
+          focusing_mode_ = not focusing_mode_;
+        }
       }
 
       cv::destroyAllWindows();
@@ -184,19 +279,25 @@ private:
                 cv::LINE_AA);
   }
 
-  rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr img_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr img_sub_;
   rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
   rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gps_sub_;
+  rclcpp::Client<rosbag2_interfaces::srv::Resume>::SharedPtr resume_;
+  rclcpp::Client<rosbag2_interfaces::srv::Pause>::SharedPtr pause_;
 
   FPSMeter img_fps_{10};
   FPSMeter imu_fps_{100};
   FPSMeter gps_fps_{50};
 
   std::unique_ptr<std::thread> wt_;
-  std::deque<sensor_msgs::msg::CompressedImage::UniquePtr> img_queue_;
+  std::deque<sensor_msgs::msg::Image::UniquePtr> img_queue_;
   std::mutex img_protector_;
   std::atomic_bool stop_requested_{false};
   std::atomic_bool gps_is_valid_{false};
+  std::atomic_bool focusing_mode_{false};
+  std::atomic_bool record_started_{false};
+
+  cv::Size img_size_;
 };
 
 int main(const int argc, const char *const *argv) {
